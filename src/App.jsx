ba60343bf1d8e9340a9382353bb223ff469e2168
Gabriel Lima <gabriellpbiel@hotmail.com>
2023-06-23 23:12:31 -0300

    criando componente app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Calculadora from './main/Calculator'
+
+export default function App(){
+    return(
+        <div>
+            <h1>Calculadora</h1>
+            <Calculadora />
+        </div>
+    )
+}
